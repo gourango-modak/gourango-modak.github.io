2de2bfb0eacf64ad0ccce950fe8bae32a7ae78d3
gourango_modak <gourango.modak.2@gmail.com>
2021-03-09 20:02:10 +0600

    Skills and Navbar updated

--- js/style.js
+++ js/style.js
@@ -4,5 +4,41 @@ $(document).ready(function(){
     $('.navbar-nav>li>a').on('click', function(){
         $('.navbar-collapse').collapse('hide');
     });
+    
+    $(window).on('load resize', function(){
+        var winHeight = $(window).height();
+        var winWidth = $(window).width();
+        if(winWidth < 495) {
+            $(".skill_cir").css("width", "100px");
+            $(".skill_text").css("font-size", "12px");
+            $('.dial').trigger(
+                'configure',
+                {
+                    "width" : 80,
+                    "height" : 80
+                }
+            );
+        }
+        else {
+            $(".skill_cir").css("width", "150px");
+            $(".skill_text").css("font-size", "18px");
+            $('.dial').trigger(
+                'configure',
+                {
+                    "width" : 120,
+                    "height" : 120
+                }
+            );
+        }
+    });
+    
+    $(".dial").knob({
+        "readOnly":true,
+        "width" : 150,
+        "height" : 150,
+        draw: function () {
+            $(this.i).val(this.cv + '%');
+        }
+    });
 });
 
